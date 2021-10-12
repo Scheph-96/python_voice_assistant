d8e13724c7496ecdda6cb07c9f1964ad3be6eda8
Scheph <omarscheph18@gmail.com>
2021-10-12 00:03:34 +0000

    Update Helena.py

--- voice_assistant/util/Helena.py
+++ voice_assistant/util/Helena.py
@@ -1,4 +1,6 @@
 # import all necessary modules
+import time
+
 import pyttsx3
 import datetime
 import speech_recognition as sr
@@ -15,30 +17,31 @@ from pygame import mixer
 
 class Helena:
     def __init__(self):
-        engine = pyttsx3.init()
-        engine.setProperty('rate', 150)
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 150)
 
     # Initialize the voice
     # If you don't have this one, it will select the default voice on your computer
-    def change_voice(self, engine, language, gender='VoiceGenderFemale'):
+    @staticmethod
+    def change_voice(engine):
         for voice in engine.getProperty('voices'):
             if voice.name == "Microsoft Zira Desktop - English (United States)":
                 engine.setProperty('voice', voice.id)
                 break
 
-    # Speaking funtion
+    # Speaking function
     def speak(self, audio):
-        change_voice(engine, language=None, gender=None)
-        engine.say(audio)
-        engine.runAndWait()
+        self.change_voice(self.engine)
+        self.engine.say(audio)
+        self.engine.runAndWait()
 
     # Presentation function
     def who_am_i(self):
-        identity = "I am Héléna your voice assistant develop by Omar on Thursday September 30, 2021"
-        speak(identity)
+        identity = "I am Helena your voice assistant develop by Omar on Thursday September 30, 2021"
+        self.speak(identity)
 
     # Order listening function
-    def takeCommand(self):
+    def take_command(self):
         recognize = sr.Recognizer()
         with sr.Microphone() as source:
             mixer.init()
@@ -47,48 +50,79 @@ class Helena:
             recognize.adjust_for_ambient_noise(source)
             audio = recognize.listen(source)
 
+        try:
+            print("Recognizing...")
+            query = recognize.recognize_google(audio)
+        except Exception as e:
+            print(e)
+            self.speak("I do not understand what you are saying")
+
+            return "None"
+
+        return query
+
     # User data writting function
     def user_data(self):
-        speak("Can i know your name ?")
+        self.speak("May i know your name ?")
+        username = self.take_command()
         data = open("data.txt", "w")
-        data.write(data)
+        data.write(username)
         data.close()
 
+    def greet(self):
+
+        hour = datetime.datetime.now().hour
+
+        if 6 <= hour < 12:
+            self.speak("Good morning")
+        elif 12 <= hour < 18:
+            self.speak("Good afternoon")
+        elif 18 <= hour < 24:
+            self.speak("Good evening")
+
     # Return current time
-    def time(self):
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-        speak("It's "+time)
+    def current_time(self):
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        self.speak("It's " + current_time)
 
     # Return current date
-    def date(self):
-        date = datetime.datetime.now().strftime("%A %d %B %Y")
-        speak("Today we are "+date)
+    def current_date(self):
+        current_date = datetime.datetime.now().strftime("%A %d %B %Y")
+        self.speak("Today's date is " + current_date)
 
+    def wikipedia_search(self):
+        self.speak("what should I look for?")
+        query = self.take_command().lower()
+        self.speak("Searching for "+query)
+        wikipedia.set_lang("en")
+        time.sleep(0.3)
+        result = wikipedia.summary(query, sentences=2)
+        self.speak(result)
 
-    def wishme(self):
+    def google_search(self):
+        self.speak("what should i look for?")
+        query = self.take_command().lower()
+        self.speak("Searching for "+query)
+        wb.open(query)
 
-        hour = datetime.datetime.now()
+    def to_remember(self, memory):
+        self.speak("What should i remember ?")
+        memoryCenter = open("memory_center", "a")
+        memoryCenter.write(memory)
+        memoryCenter.close()
+        self.speak("You told me to remember " + memory)
 
-        if hour >= 6 and hour < 12:
-            speak("Good morning")
-        elif hour >= 12 and hour <18:
-            speak("Good afternoon")
-        elif hour >= 18 and hour < 24:
-            speak("Good evening")
+    def shutdown(self):
+        self.speak("Shutting down the computer")
+        os.system("shutdown /s /t 1")
 
+    def restart(self):
+        self.speak("Reboot")
+        os.system("shutdown /r /t 1")
 
-     def toRemember(self):
-         speak("What shoud i remember ?")
-         memory = self.takeCommand()
-         memoryCenter = open("memory_center", "a")
-         memoryCenter.write(memory)
-         memoryCenter.close()
-         speak("You told me to remember "+memory)
-
-
-     def shutdown(self):
-         speak(Sutting down the computer)
-         os.system("shutdown")
+    def logout(self):
+        self.speak("Logout")
+        os.system("shutdown -l")
 
 
 if __name__ == "__module__":
